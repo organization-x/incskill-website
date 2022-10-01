3edbd440b38d7b43afcaac4ff5df3ee98577a8d0
m-satler <79671074+m-satler@users.noreply.github.com>
2022-10-01 12:02:17 -0500

    added gitignore

--- login_page_backend/accounts/models.py
+++ login_page_backend/accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+
+class User(models.Model):
+    username = models.CharField(max_length=30, unique=True)
+    password = models.CharField(max_length=40)
+    is_staff = models.BooleanField()
+    is_superuser = models.BooleanField()
+
+    def __str__(self):
+        return self.name()
 
