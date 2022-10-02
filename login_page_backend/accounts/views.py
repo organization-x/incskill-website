64fb699fbe337c0d034ad2f5932e9f72d2a3a34e
Hilbs100 <57808200+Hilbs100@users.noreply.github.com>
2022-10-01 21:44:10 -0400

    Got login page to be the first page users see (Django)
    Code is still not fully working, but the first page users see will be the login page. Still haven't gotten it to go to the home page after logging in, though

--- login_page_backend/accounts/views.py
+++ login_page_backend/accounts/views.py
@@ -1,9 +1,15 @@
+from http.client import HTTPResponse
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from django.shortcuts import render
+from django.http import HttpResponse
 
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy("login")
-    template_name = "registration/signup.html"
+    success_url = reverse_lazy("loginPage")
+    template_name = "Register.html"
+
+def home_view(request, *args, **kwargs):
+    return HTTPResponse("loginPage.html")
