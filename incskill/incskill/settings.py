906176085c81b4e67217a3cf8ab51fea172372d7
m-satler <79671074+m-satler@users.noreply.github.com>
2022-10-12 18:58:58 -0500

    Revert "Deployment Prep"
    This reverts commit b333fd68b95f5305488a202cceb4fe34a10adf39.

--- incskill/incskill/settings.py
+++ incskill/incskill/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-*#5q4#+-nlt#g897^+fibf^f7we7ych#+9q_8sti07_9m+a7a0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
