637f7e77eab939f6c7d53a693d65e6807220d2af
m-satler <79671074+m-satler@users.noreply.github.com>
2022-10-31 17:36:00 -0500

    railway deployment 2/2
    should have all files needed for deployment to railway

---
+++ README.md
@@ -0,0 +1,16 @@
+# IncSkill Website
+## Table of Contents
+* [Introduction](#introduction)
+* [Tech Stack](#tech-stack)
+* [Project Status](#project-status)
+* [Launch](#launch)
+
+## Introduction
+IncSkill is a website that was commissioned by the Washington State Department of the Blind. Its goal is to create an accessible portal for learning about Computer Science with a focus on accessibility for users with visual impariments. 
+You can learn more [here](https://aicamp.notion.site/IncSkill-Website-Product-Spec-f59270ae31b24c1daad56ec74b821b50).
+## Tech Stack
+We will be using Amazon web services to deploy and host our project. We’re using Django for Python as our backend framework. We chose this instead of JS because our engineers are more familiar with Python than JS. For the frontend, we’re using Bootstrap.
+## Project Status
+The website is still in development, however, the website is now mildly accessible to users. Currently we have a login, signup, and courses page established and a Navigation Bar to go between these pages. Within the next few weeks, more features will be rolled out, including styling, screenreader compatibility, and progress bars for users. 
+## Launch
+Currently, there are several ways to open the website, the first, and primary method for accessing the site should be by opening a live server from loginPage.html or starting it through the terminal with the command 'start loginPage.html'. 
---
+++ incskill/urls.py
@@ -0,0 +1,64 @@
+"""incskill URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from incskill_site.views import (
+    LoginView,
+    CoursePageView,
+    SignUpView,
+    ProfileView,
+    CourseOneView,
+    ResourceOneView,
+    ResourceTwoView,
+    ResourceThreeView,
+    ResourceFourView,
+    ResourceFiveView,
+    ResourceSixView,
+    ResourceSevenView,
+    ResourceEightView,
+    ResourceNineView,
+    ResourceTenView,
+    ResourceElevenView,
+    ResourceTwelveView,
+    QuizOneView,
+    CodingExcerciseView, 
+    )
+import incskill_site.views as views
+
+
+urlpatterns = [
+    path('', LoginView.as_view(), name = 'login'),
+    path('courses/', CoursePageView.as_view(), name = 'courses'),
+    path('signup/', SignUpView.as_view(), name = "signup"),
+    path('courses/course-one/', CourseOneView.as_view(), name = 'course_one'),
+    path('profile/', ProfileView.as_view(), name = 'profile'),
+    path('admin/', admin.site.urls),
+    path('courses/course-one/resource-one', ResourceOneView.as_view(), name = 'resourceone'),
+    path('courses/course-one/resource-two', ResourceTwoView.as_view(), name = 'resourcetwo'),
+    path('courses/course-one/resource-three', ResourceThreeView.as_view(), name = 'resourcethree'),
+    path('courses/course-one/resource-four', ResourceFourView.as_view(), name = 'resourcefour'),
+    path('courses/course-one/resource-five', ResourceFiveView.as_view(), name = 'resourcefive'),
+    path('courses/course-one/resource-six', ResourceSixView.as_view(), name = 'resourcesix'),
+    path('courses/course-one/resource-seven', ResourceSevenView.as_view(), name = 'resourceseven'),
+    path('courses/course-one/resource-eight', ResourceEightView.as_view(), name = 'resourceeight'),
+    path('courses/course-one/resource-nine', ResourceNineView.as_view(), name = 'resourcenine'),
+    path('courses/course-one/resource-ten', ResourceTenView.as_view(), name = 'resourceten'),
+    path('courses/course-one/resource-eleven', ResourceElevenView.as_view(), name = 'resourceeleven'),
+    path('courses/course-one/resource-twelve', ResourceTwelveView.as_view(), name = 'resourcetwelve'),
+    path('courses/course-one/quiz-one', QuizOneView.as_view(), name = 'quizone'),
+    path('courses/course-one/quiz-one/update-score/', views.score_updater , name="score_updater"),
+    path('courses/course-one/coding-excercise', CodingExcerciseView.as_view(), name = 'codingexcercise')
+]
